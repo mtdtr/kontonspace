35d5b3e2a0515376cd73dfb92b6564a1446a25cf
MTD <dev.mantendo@gmail.com>
2017-12-17 22:05:44 +0900

    Add files via upload
    Add profile for new members.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//アニメーション
 id = setInterval( function(){
   $('.wrapper').removeClass('opnone');
 }, 250);
@@ -6,14 +7,28 @@ setTimeout( function(){
     clearInterval(id);
 }, 1000);
 
+//プロフィール
 function mtdshow(){
-  document.getElementById('membervoid').innerHTML = '<h3>MTDTR</h3><p>このサークルの主宰者。どうしようもない系廃人。</p><p>産業廃棄物みたいなこのサイトを作った犯人はこいつです。</p><p>ピアノを弾いてるのもこいつです。</p><p><del>本当は一人なのに同人サークルとか言ってるのもこいつです。</del><b>祝!ぼっちサークル卒業!</b></p><p><a href="https://twitter.com/MTDTR_tk" target="_blank"><img class="menu" height="50px" src="img/twitterlogo.png" alt="twitter" title=この廃人のツイッターを訪ねる></a><a href="http://mstdn.jp/@MTDTR" target="_blank"><img class="menu" height="40px" src="https://mstdn.jp/favicon.ico" alt="mstdn" title=この廃人のマストドンを訪ねる></a><a href="https://github.com/MTDTR" target="_blank"><img class="menu" height="50px" src="img/github.png" alt="github" title=この廃人のGitHubを訪ねる></a>';
+  document.getElementById('membervoid').innerHTML = '<h3>MTDTR</h3><p class="small"><i>a.k.a.あきやまあかね</i></p><p>このサークルの主宰者。どうしようもない系廃人。</p><p>産業廃棄物みたいなこのサイトを作った犯人はこいつです。</p><p>ピアノを弾いてるのもこいつです。</p><p><del>本当は一人なのに同人サークルとか言ってるのもこいつです。</del><b>祝!ぼっちサークル卒業!</b></p><p><span style="background-color:#777777;color:#ffffff;">担当楽器:ピアノ</span></p><p><a href="https://twitter.com/MTDTR_tk" target="_blank"><img class="menu" height="50px" src="img/twitterlogo.png" alt="twitter" title=この廃人のツイッターを訪ねる></a><a href="http://mstdn.jp/@MTDTR" target="_blank"><img class="menu" height="40px" src="https://mstdn.jp/favicon.ico" alt="mstdn" title=この廃人のマストドンを訪ねる></a><a href="https://github.com/MTDTR" target="_blank"><img class="menu" height="50px" src="img/github.png" alt="github" title=この廃人のGitHubを訪ねる></a>';
 }
 
 function pianoshow(){
   document.getElementById('membervoid').innerHTML = '<h3>ピアノ</h3><p>ろーらんど王国生まれの電子ピアノ。</p><p>このサークルに絶対必要なメンバー。</p><p>初期にはこの人がいないと同人サークルとして成立しなかったんだよね。。。</p>';
 }
 
+function genkanshow(){
+  document.getElementById('membervoid').innerHTML = '<h3>玄関</h3><p class="small"><i>a.k.a.魚好きよ</i></p><p>ラ<br>ララ<br>ら・ララランド<br>魚が好き(本人談)</p><p>当サークル専属メイドさん。</p><p><span style="background-color:#777777;color:#ffffff;">担当楽器:サイリウム</span></p>';
+}
+
+function domashow(){
+  document.getElementById('membervoid').innerHTML = '<h3>土間</h3><p class="small"><i>a.k.a.シャロリング</i></p><p>当サークル専属絵師にして3人目のメンバー(ピアノ含む)。アイコンは本人が描いてくれました!</p><p><span class="small" style="color:#999999;">某</span>和太鼓<span class="small" style="color:#999999;">風ゲーム</span>8段取得。</p><p><span style="background-color:#777777;color:#ffffff;">担当楽器:和太鼓・サイリウム・ピアノ(予定)</span></p>';
+}
+
+function kutsubakoshow(){
+  document.getElementById('membervoid').innerHTML = '<h3>玄関</h3><p class="small"><i>a.k.a.ふぃぎゅ</i></p><p>保健の成績は5だけど体育の成績は1。(本人談)</p><p>そろばん(マラカス)初段取得。</p><p>アイコンは土間さんが描いてくれました。</p><p><span style="background-color:#777777;color:#ffffff;">担当楽器:そろばんマラカス・サイリウム</span></p>';
+}
+
+//メルマガ関連(必要か?)
 function merumagaen(){
   document.getElementById('submit').innerHTML = "<input type='submit' value='規約に同意して進む'>"
 }
@@ -22,6 +37,7 @@ function merumagadis(){
   document.getElementById('submit').innerHTML = "<input type='submit' value='次へ'>"
 }
 
+//オープニングアニメーション
 function kontonindex(){
   $('.indexwrapper').toggleClass('indexactive');
   $('.center').toggleClass('centeractive');
